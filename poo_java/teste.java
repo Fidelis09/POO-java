7e1873ce905fd005e910f1ef047505060d42cb14
Luis Otavio Fatore Madeira <137592597+l0izim@users.noreply.github.com>
2025-02-05 19:57:43 -0300

    Update teste.java

--- poo_java/teste.java
+++ poo_java/teste.java
@@ -2,7 +2,7 @@ public class teste {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-}
+}//Luis Otavio diz oi pro mundo :>
 public class teste {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
